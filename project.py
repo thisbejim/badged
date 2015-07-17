8442c22a7f41f3d350395b158ea5a0999e083f3f
thisbejim <jimmy_cm3@hotmail.com>
2015-07-17 17:24:08 +1000

    basic functionality all round

--- project.py
+++ project.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from flask import Flask, render_template, Markup, request, redirect, url_for, jsonify
+from flask import Flask, render_template, session, request, redirect, url_for, jsonify, Response
 import json
 import logging
 import string
@@ -43,6 +43,34 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
+def decode_assertion(assertion):
+    with open('static/public-key.pem', 'r') as rsa_file:
+        pub_key = rsa_file.read()
+        decoded = jwt.decode(assertion, pub_key, algorithm=['RS256'])
+        return decoded
+
+
+def build_assertion(email, badge_url):
+    time_now = time.time()
+    time_now = int(time_now)
+    assertion = {"uid": id_generator(),
+                 "recipient": {"identity": email, "type": "email", "hashed": False},
+                 "badge": badge_url,
+                 "verify": {"url": "https://badged.herokuapp.com/static/public-key.pem", "type": "signed"},
+                 "issuedOn": time_now}
+
+    # Sign with private key
+    with open('static/private-key.pem', 'r') as rsa_file:
+        key = rsa_file.read()
+    encoded = jwt.encode(assertion, key, algorithm='RS256', headers={'alg': 'RS256'})
+
+    # Check a decode with public key
+    with open('static/public-key.pem', 'r') as rsa_file:
+        pub_key = rsa_file.read()
+        decoded = jwt.decode(encoded, pub_key, algorithm=['RS256'])
+
+    return encoded.decode("utf-8")
+
 # List all assets
 @app.route('/')
 def index():
@@ -52,44 +80,58 @@ def index():
 @app.route('/check', methods=['GET', 'POST'])
 def check():
     if request.method == 'POST':
-            count = 0
+            assertions = []
             for status in tweepy.Cursor(api.user_timeline, user_id=request.form['handle']).items(400):
-                print(status)
-                count+=1
-            print("count is HERE!")
-            print(count)
+                if "#tech" in status.text:
+                    badge = {}
+                    badge["img"] = "badges/#securework/#securework.png"
+                    badge["assertion"] = build_assertion(request.form['email'],
+                                         "https://badged.herokuapp.com/static/badges/#securework/badge.json")
+                    badge["tweet"] = status.text
+                    assertions.append(badge)
+                if "#javascript" in status.text:
+                    badge = {}
+                    badge["img"] = "badges/#highered/#highered.png"
+                    badge["assertion"] = build_assertion(request.form['email'],
+                                         "https://badged.herokuapp.com/static/badges/#highered/badge.json")
+                    badge["tweet"] = status.text
+                    assertions.append(badge)
+                if "#rpl" in status.text:
+                    badge = {}
+                    badge["img"] = "badges/#rpl/#rpl.png"
+                    badge["assertion"] = build_assertion(request.form['email'],
+                                         "https://badged.herokuapp.com/static/badges/#rpl/badge.json")
+                    badge["tweet"] = status.text
+                    assertions.append(badge)
+                if "#lifelonglearning" in status.text:
+                    badge = {}
+                    badge["img"] = "badges/#lifelonglearning/#lifelonglearning.png"
+                    badge["assertion"] = build_assertion(request.form['email'],
+                                         "https://badged.herokuapp.com/static/badges/#lifelonglearning/badge.json")
+                    badge["tweet"] = status.text
+                    assertions.append(badge)
+                if "#Auspol" in status.text:
+                    badge = {}
+                    badge["img"] = "badges/#Auspol/#Auspol.png"
+                    badge["assertion"] = build_assertion(request.form['email'],
+                                         "https://badged.herokuapp.com/static/badges/#Auspol/badge.json")
+                    badge["tweet"] = status.text
+                    assertions.append(badge)
 
-            time_now = time.time()
-            time_now = int(time_now)
-
-            # Generate assertion
-            assertion = {"uid": id_generator(),
-                         "recipient": {"identity": request.form['email'], "type": "email", "hashed": False},
-                         "badge": "https://badged.herokuapp.com/static/badges/badge.json",
-                         "verify": {"url": "https://badged.herokuapp.com/static/public-key.pem", "type": "signed"},
-                         "issuedOn": time_now}
-
-            # Sign with private key
-            with open('static/private-key.pem', 'r') as rsa_file:
-                key = rsa_file.read()
-            encoded = jwt.encode(assertion, key, algorithm='RS256',
-                                 headers={'alg': 'RS256'})
-
-            # Check a decode with public key
-            with open('static/public-key.pem', 'r') as rsa_file:
-                pub_key = rsa_file.read()
-            decoded = jwt.decode(encoded, pub_key, algorithm=['RS256'])
-
-            return encoded
+            session['assertions'] = assertions
+            print(session['assertions'])
+            return "200"
 
     return render_template('check.html')
 
 
 
-@app.route('/award/<assertion>')
-def award(assertion):
-
-    return render_template('award.html', assertion=assertion)
+@app.route('/award/')
+def award():
+    print("assertions")
+    print(session['assertions'])
+    lol = session['assertions']
+    return render_template('award.html', assertions=lol)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
